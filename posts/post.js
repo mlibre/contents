2ad7ca1518a9b1071dc3bb85ceb57fb9ce1e83bc
mlibre <m.gh@linux.org>
2021-03-11 19:16:44 +0330

    new post

---
+++ posts/post.js
@@ -0,0 +1,11 @@
+# Fix Headphone and microphone detection problem on Linux, AMD
+
+[![Fix Headphone and microphone detection problem on Linux, AMD](http://img.youtube.com/vi/DGNxHNHBiQ8/0.jpg)](https://www.youtube.com/watch?v=DGNxHNHBiQ8 "Fix Headphone and microphone detection problem on Linux, AMD")
+
+
+How to Fix Headphone, Microphone or Handsfree detection problem on Linux, AMD. 2019. Ubuntu 18, Mint 19<br/>	commands:<br/>	sudo apt-get install alsa-base alsa-tools alsa-tools-gui alsa-utils<br/>	# ....
+
+https://lbry.tv/@mlibre:e/fix-headphone-and-microphone-detection:f
+
+ETH Address:
+> 0xc9b64496986E7b6D4A68fDF69eF132A35e91838e
